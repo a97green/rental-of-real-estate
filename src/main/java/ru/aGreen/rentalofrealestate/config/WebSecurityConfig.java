af0b9a3da3504708dd28fd3f15b7079dd13ebff8
a97green <a97green@gmail.com>
2021-10-23 13:56:43 +0700

    страница с подробностями о квартире

--- src/main/java/ru/aGreen/rentalofrealestate/config/WebSecurityConfig.java
+++ src/main/java/ru/aGreen/rentalofrealestate/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .csrf()
             .disable()
             .authorizeRequests()
-            .antMatchers("/assets/**", "/css/**", "/fonts/**", "/js/**", "/img/**", "/").permitAll()
+            .antMatchers("/assets/**", "/css/**", "/fonts/**", "/js/**", "/img/**", "/", "/apartment/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
--- src/main/java/ru/aGreen/rentalofrealestate/controllers/MainController.java
+++ src/main/java/ru/aGreen/rentalofrealestate/controllers/MainController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import ru.aGreen.rentalofrealestate.models.*;
 import ru.aGreen.rentalofrealestate.repositories.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class MainController {
@@ -41,6 +45,25 @@ public class MainController {
         model.addAttribute("apartaments", apartaments);
         model.addAttribute("attractions", attractions);
         return "index-parallax";
+    }
+
+    @GetMapping("/apartment/{id}")
+    public String apartment(@PathVariable(value = "id") Long id, Model model) {
+        Iterable<HomeHeaders> homeHeaders = homeHeadersRepository.findAll();
+        for (HomeHeaders headers : homeHeaders) {
+            model.addAttribute("homeHeaders", headers);
+        }
+        Iterable<Apartaments> apartaments = apartamentsRepository.findAll();
+        Apartaments apartament = apartamentsRepository.findById(id).orElseThrow(() -> new NoSuchElementException(""));
+        List<String> strings = Images.disband(apartament.getPhoto());
+        List<Integer> integers = new ArrayList<>();
+        AtomicInteger i = new AtomicInteger();
+        strings.forEach(e -> integers.add(i.getAndIncrement()));
+        model.addAttribute("apartament", apartament);
+        model.addAttribute("apartaments", apartaments);
+        model.addAttribute("strings", strings);
+        model.addAttribute("integers", integers);
+        return "index-form";
     }
 
     @GetMapping("/error")
